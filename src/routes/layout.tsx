f6fe1f461e5cdf42fd7591f4428c4a93ee524f7f
CrappyDill <mddillon47@gmail.com>
2022-10-16 18:08:30 -0700

    homepage layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,20 +1,22 @@
-import { component$, Slot } from '@builder.io/qwik';
-import Header from '../components/header/header';
+import { component$ } from '@builder.io/qwik';
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <>
       <main>
-        <Header />
-        <section>
-          <Slot />
-        </section>
+        <div class="min-h-screen grid grid-flow-col bg-slate-300">
+          <div class="col-span-3 grid content-evenly justify-center place-items-center border-r-4 border-indigo-500">
+            <h1 class="">Matthew Dillon</h1>
+            <Link class="">About</Link>
+            <Link class="">Skills</Link>
+            <Link class="">Projects</Link>
+            <Link class="">Experience</Link>
+          </div>
+
+          <div class="col-span-9">Probably a Slot</div>
+        </div>
       </main>
-      <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-        </a>
-      </footer>
     </>
   );
 });
